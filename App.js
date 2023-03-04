3484d401b4b6f4d06d467a826a2f3ea61f3930b5
AbhijeetSR9996 <sabhijeet191@yahoo.com>
2023-03-04 23:13:25 +0530

    Add all my files

---
+++ App.js
@@ -0,0 +1,54 @@
+//Import react-navigation
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
+
+//Import external files
+import HomeScreen from './src/HomeScreen';
+import RegisterUser from './src/RegisterUser';
+import UpdateUser from './src/UpdateUser';
+import ViewUser from './src/ViewUser';
+import DeleteUser from './src/DeleteUser';
+
+const App = createStackNavigator({
+  HomeScreen: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: 'HomeScreen',
+      headerStyle: { backgroundColor: '#3a59b7' },
+      headerTintColor: '#ffffff',
+    },
+  },
+  View: {
+    screen: ViewUser,
+    navigationOptions: {
+      title: 'View User',
+      headerStyle: { backgroundColor: '#3a59b7' },
+      headerTintColor: '#ffffff',
+    },
+  },
+  Update: {
+    screen: UpdateUser,
+    navigationOptions: {
+      title: 'Update User',
+      headerStyle: { backgroundColor: '#3a59b7' },
+      headerTintColor: '#ffffff',
+    },
+  },
+  Register: {
+    screen: RegisterUser,
+    navigationOptions: {
+      title: 'Register User',
+      headerStyle: { backgroundColor: '#3a59b7' },
+      headerTintColor: '#ffffff',
+    },
+  },
+  Delete: {
+    screen: DeleteUser,
+    navigationOptions: {
+      title: 'Delete User',
+      headerStyle: { backgroundColor: '#3a59b7' },
+      headerTintColor: '#ffffff',
+    },
+  },
+});
+export default createAppContainer(App);
